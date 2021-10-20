da36513bd6469d4955909cfcc5f6ea2b8d0872fb
stas-shr <stasshr@gmail.com>
2021-10-20 15:59:34 +0300

    Выполнил задание 4.9. Больше деталей

--- js/main.js
+++ js/main.js
@@ -1,12 +1,111 @@
+/* eslint-disable no-unused-vars */
+/* eslint-disable no-console */
 // src https://myrusakov.ru/js-random-numbers.html
 function getRandomInRange(min, max) {
   return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
-getRandomInRange(1, 10);
-
 // src https://myrusakov.ru/js-random-numbers.html с доработкой из https://stackoverflow.com/questions/17726753/get-a-random-number-between-0-0200-and-0-120-float-numbers
 function getRandomFloat(min, max, floatNuber) {
   return (Math.random() * (max - min) + min).toFixed(floatNuber);
 }
-getRandomFloat(11, 101, 2);
+
+function getRandom(random) {
+  return Math.floor(Math.random() * random);
+}
+
+const avatar = [
+  'img/avatars/user01.png',
+  'img/avatars/user02.png',
+  'img/avatars/user03.png',
+  'img/avatars/user04.png',
+  'img/avatars/user05.png',
+  'img/avatars/user06.png',
+  'img/avatars/user07.png',
+  'img/avatars/user08.png',
+  'img/avatars/user09.png',
+  'img/avatars/user10.png',
+];
+
+const type = [
+  'palace',
+  'flat',
+  'house',
+  'bungalow',
+  'hotel',
+];
+
+const checkin = [
+  '12:00',
+  '13:00',
+  '14:00',
+];
+
+const checkout = [
+  '12:00',
+  '13:00',
+  '14:00',
+];
+
+const allFeatures = [
+  'wifi',
+  'dishwasher',
+  'parking',
+  'washer',
+  'elevator',
+  'conditioner',
+];
+
+function getArray(elements) {
+  const maxElements = elements.length;
+  const lengthOfArray = getRandomInRange(1, maxElements);
+  const newArray = [];
+
+  while (newArray.length < lengthOfArray) {
+    const indexOfElement = getRandomInRange(0, maxElements - 1);
+    const element = elements[indexOfElement];
+
+    if (!newArray.includes(element)) {
+      newArray.push(element);
+    }
+  }
+  return newArray;
+}
+
+const allPhotos = [
+  'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/duonguyen-8LrGtIxxa4w.jpg',
+  'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/brandon-hoogenboom-SNxQGWxZQi0.jpg',
+  'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/claire-rendall-b6kAwr1i0Iw.jpg',
+];
+
+
+const getRandomArrayElement = (elements) => elements[_.random(0, elements.length - 1)];
+
+const OBJ_COUNT = 10;
+
+const createObj = () => ({
+  author: {
+    avatar: avatar.shift(),
+  },
+  offer: {
+    title: 'Аренда помещения',
+    address: `Адрес по координатам: ${  getRandomFloat(35.65000, 35.70000, 5)  } и ${  getRandomFloat(139.70000, 139.80000, 5)}`,
+    price: getRandomInRange(1000, 10000),
+    type: getRandomArrayElement(type),
+    rooms: getRandomInRange(1, 5),
+    guests: getRandomInRange(2, 7),
+    checkin: getRandomArrayElement(checkin),
+    checkout: getRandomArrayElement(checkout),
+    features: getArray(allFeatures),
+    description: 'Сдам уютное помещение для порядочных жильцов.',
+    photos: getArray(allPhotos),
+  },
+  location: {
+    lat: getRandomFloat(35.65000, 35.70000, 5),
+    lng: getRandomFloat(139.70000, 139.80000, 5),
+  },
+});
+
+const similarObj = Array.from({length: OBJ_COUNT}, createObj);
+
+console.log(similarObj);
